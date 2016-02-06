15b0105d58705d67a46335558acac737112ee202
subeesh <subeesh.a@gmail.com>
2016-02-06 12:20:10 +0800

    states

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  render(){
-    return <div>Hello World</div>
+
+  constructor() {
+    super();
+
+    this.state = { message : 'Hello World'}
+  }
+
+  update(e) {
+  	this.setState({
+  		message : e.target.value
+  	})
+  }
+
+  render() {
+    return (
+    	<div>
+    		<input onChange={this.update.bind(this)}/>
+    		<div>{this.state.message}</div>
+    	</div>
+		)
   }
 }
