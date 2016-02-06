eee0414ecffb9c13bf33f4454413b4e588146279
subeesh <subeesh.a@gmail.com>
2016-02-06 18:14:46 +0800

    simple list

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import RestaurantList from './components/RestaurantList';
 
 export default class App extends React.Component {
 
   constructor() {
     super();
-
-    this.state = { message : 'Hello World'}
-  }
-
-  update(e) {
-  	this.setState({
-  		message : e.target.value
-  	})
   }
 
   render() {
     return (
     	<div>
-    		<input onChange={this.update.bind(this)}/>
-    		<div>{this.state.message}</div>
+    		<RestaurantList />
     	</div>
 		)
   }
---
+++ components/RestaurantList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './RestaurantCard';
+
+export default class RestaurantList extends React.Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			restaurants: [1,2,3,4,5,6,7,8]
+		}
+	}
+
+
+	render() {
+		return (
+			<div>{this.state.restaurants.map((d) => {
+					return <div key={d}>{d}</div>
+				})}
+			</div>
+		)
+	}
+}
