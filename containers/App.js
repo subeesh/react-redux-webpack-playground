61f9f213e896c049cceb8cd7ff2296a32e986aaf
subeesh <subeesh.a@gmail.com>
2016-02-07 23:00:26 +0800

    add last updated and refresh button

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { fetchRestaurantsIfNeeded } from '../actions'
+import { fetchRestaurantsIfNeeded,
+  invalidateLocation
+} from '../actions'
 
 import RestaurantList from '../components/RestaurantList'
 
@@ -9,19 +11,47 @@ class App extends Component {
 
   constructor(props) {
     super(props)
-    console.log(this.props)
+    this.handleRefreshClick = this.handleRefreshClick.bind(this)
   }
 
   componentDidMount() {
     const { selectedLocation, dispatch } = this.props
-    console.log(selectedLocation)
+    dispatch(fetchRestaurantsIfNeeded(selectedLocation))
+  }
+
+  handleRefreshClick(event) {
+    event.preventDefault()
+
+    const { selectedLocation, dispatch } = this.props
+    dispatch(invalidateLocation(selectedLocation))
     dispatch(fetchRestaurantsIfNeeded(selectedLocation))
   }
 
   render() {
+    const { lastUpdated, isFetching, items } = this.props
+    const isEmpty = items.length == 0
     return (
       <div>
-        <RestaurantList items={this.props.items} />
+        <p>
+          {lastUpdated &&
+            <span>
+              Last Updated at {new Date(lastUpdated).toLocaleTimeString()}.
+              {' '}
+            </span>
+          }
+          {!isFetching &&
+            <a href='#'
+              onClick={this.handleRefreshClick}>
+              Refresh
+            </a>
+          }
+        </p>
+        {isEmpty
+          ? (isFetching ? <h3>Loading...</h3> : <h3>Empty.</h3>)
+          : <div style={{opacity: isFetching ? .2 : 1}}>
+              <RestaurantList items={this.props.items} />
+            </div>
+        }
       </div>
     )
   }
